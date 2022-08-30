648550a35de9b8a79d064a55caa5805e6590a507
fatur.ewing <fatur.ewing@sicepat.com>
2022-08-30 20:15:37 +0700

    Remove goroutine method

--- main.go
+++ main.go
@@ -3,36 +3,47 @@ package main
 import (
 	"fmt"
 	"hacktiv8/latihan"
-	"sync"
 )
 
 func main() {
 	var db []*latihan.User
-	userSvc := latihan.NewUserService(db)
+	user := latihan.NewUserService(db)
 	names := []string{"Ucup", "Wahyono", "Jamilah", "Simorang", "Cahyo", "Andin", "Clara"}
-	var wg1 sync.WaitGroup
-	wg1.Add(len(names))
 	for _, n := range names {
-		go func(name string) {
-			res := userSvc.Register(&latihan.User{Nama: name})
-			fmt.Println(res)
-			wg1.Done()
-		}(n)
+		res := user.Register(&latihan.User{Nama: n})
+		fmt.Println(res)
 	}
-	wg1.Wait()
 
-	resGet := userSvc.GetUser()
+	getUser := user.GetUser()
 	fmt.Println("________ GET USER ________")
-	var wg sync.WaitGroup
-	wg.Add(len(resGet))
-	for _, v := range resGet {
-		go cetakNama(&wg, v.Nama)
+
+	for _, v := range getUser {
+		fmt.Println(v.Nama)
 	}
-	wg.Wait()
+
+	// var wg1 sync.WaitGroup
+	// wg1.Add(len(names))
+	// for _, n := range names {
+	// 	go func(name string) {
+	// 		res := userSvc.Register(&latihan.User{Nama: name})
+	// 		fmt.Println(res)
+	// 		wg1.Done()
+	// 	}(n)
+	// }
+	// wg1.Wait()
+
+	// resGet := userSvc.GetUser()
+	// fmt.Println("________ GET USER ________")
+	// var wg sync.WaitGroup
+	// wg.Add(len(resGet))
+	// for _, v := range resGet {
+	// 	go cetakNama(&wg, v.Nama)
+	// }
+	// wg.Wait()
 
 }
 
-func cetakNama(wg *sync.WaitGroup, nama string) {
-	fmt.Println(nama)
-	wg.Done()
-}
+// func cetakNama(wg *sync.WaitGroup, nama string) {
+// 	fmt.Println(nama)
+// 	wg.Done()
+// }
