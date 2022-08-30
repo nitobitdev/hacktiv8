9f47e5c918217549444dda1786543382fb17ef3b
fatur.ewing <fatur.ewing@sicepat.com>
2022-08-30 20:37:32 +0700

    Remove comment

--- main.go
+++ main.go
@@ -21,29 +21,4 @@ func main() {
 		fmt.Println(v.Nama)
 	}
 
-	// var wg1 sync.WaitGroup
-	// wg1.Add(len(names))
-	// for _, n := range names {
-	// 	go func(name string) {
-	// 		res := userSvc.Register(&latihan.User{Nama: name})
-	// 		fmt.Println(res)
-	// 		wg1.Done()
-	// 	}(n)
-	// }
-	// wg1.Wait()
-
-	// resGet := userSvc.GetUser()
-	// fmt.Println("________ GET USER ________")
-	// var wg sync.WaitGroup
-	// wg.Add(len(resGet))
-	// for _, v := range resGet {
-	// 	go cetakNama(&wg, v.Nama)
-	// }
-	// wg.Wait()
-
 }
-
-// func cetakNama(wg *sync.WaitGroup, nama string) {
-// 	fmt.Println(nama)
-// 	wg.Done()
-// }
